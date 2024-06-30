05fcc3ce12eb11e574759429d31494d829438cbd
shirin0507 <166390137+shirin0507@users.noreply.github.com>
2024-07-01 01:51:14 +0500

    1.2

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -2,24 +2,34 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\Student;
+use App\Models\FirstName;
+use App\Models\LastName;
+use App\Models\Phone;
+use App\Models\Birthday;
 use Illuminate\Support\Str;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
  */
 class StudentFactory extends Factory
 {
-
-    protected $model = Student::class;
-    public function definition()
+    public function definition(): array
     {
+        $firstName = FirstName::inRAndomOrder()->first();
+        $lastName = LastName::inRAndomOrder()->first();
+        $phone = Phone::inRAndomOrder()->first();
+        $birthday = Birthday::inRAndomOrder()->first();
+        $gender = fake()->randomElement(['male', 'female']);
+        $createdAt = fake()->dateTimeBetween('-10 months', 'now');
         return [
-            'first_name' => $this->faker->firstName,
-            'last_name' => $this->faker->lastName,
-            'phone' => $this->faker->phoneNumber,
-            'birthday' => $this->faker->date,
-            'gender' => $this->faker->randomElement(['male', 'female']),
+            'first_name' => $firstName->firstName,
+            'last_name' => $lastName->lastName,
+            'phone' => $phone->phoneNumber,
+            'birthday' => $birthday->date,
+            'gender' => Carbon::parse($gender),
+            'created_at' => Carbon::parse($createdAt),
         ];
     }
 }
