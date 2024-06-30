ed71e84b1cb5fa1ab91744dcaedbeb2ba76d9d27
shirin0507 <166390137+shirin0507@users.noreply.github.com>
2024-07-01 01:53:47 +0500

    1.3

--- database/factories/TeacherFactory.php
+++ database/factories/TeacherFactory.php
@@ -2,24 +2,22 @@
 
 namespace Database\Factories;
 
+use App\Models\FirstName;
+use App\Models\LastName;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\Teacher;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Teacher>
  */
 class TeacherFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
+    public function definition(): array
     {
+        $firstName = FirstName::inRAndomOrder()->first();
+        $lastName = LastName::inRAndomOrder()->first();
         return [
-            'first_name' => $this->faker->firstName,
-            'last_name' => $this->faker->lastName,
+            'first_name' => $firstName->firstName,
+            'last_name' => $lastName->lastName,
         ];
     }
 }
